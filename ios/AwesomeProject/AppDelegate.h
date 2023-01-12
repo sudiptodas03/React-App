5035d2c242f3cad558ce0f8701502c455fdd39dc
Sudipto Das <sudipto@singular.net>
2023-01-12 19:34:45 +0530

    First commit

---
+++ ios/AwesomeProject/AppDelegate.h
@@ -0,0 +1,10 @@
+#import <React/RCTBridgeDelegate.h>
+#import <UIKit/UIKit.h>
+#import <Singular-React-Native/SingularBridge.h>
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
+
+@end
